0c428ba1f3bff5402df967b572ebbf39be6db59e
John Hashem <jphyqr@gmail.com>
2020-04-11 16:55:37 -0600

    made backgrond red

--- pages/widgets/techStack.js
+++ pages/widgets/techStack.js
@@ -4,8 +4,16 @@ import React from 'react'
 const TechStack = () =>{
 
     return(
+        <div className='container'>
 <h3>Tech Stack</h3>
-
+    <style jsx>{`
+     .container {
+         height: 200px;
+         width: 600px;
+         background-color: red;
+     }
+    `}</style>
+</div>
     ) 
 
 }

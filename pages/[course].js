3f7dae1b32c9d257cdc891a2e0749e4cefb403a5
John Hashem <jphyqr@gmail.com>
2020-04-11 22:58:27 -0600

    print process

--- pages/[course].js
+++ pages/[course].js
@@ -2,7 +2,7 @@ import React from 'react'
 const env = 'undefined' !== process ? process.env.test : null
 
 
-console.log('process.env.test', process?.env)
+console.log('process.env.test', process)
 const Course = () => {
     return (
         <div>

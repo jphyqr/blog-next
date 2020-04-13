7331db1e0a295a92f2e2335daaccf5ba078ad45f
John Hashem <jphyqr@gmail.com>
2020-04-13 15:19:41 -0600

    added rootofPath redirect for prod

--- actions/authActions.js
+++ actions/authActions.js
@@ -6,9 +6,19 @@ export const sendEmailLink = async ({firebase}, email, redirectPath) =>{
     console.log('SEND EMAIL LINK')
     //WHERE WE DO THE HEAVY LIFTING
 try{
+
+  let rootOfPath;
+
+  if(process.NODE_ENV ==='production')
+   rootOfPath = 'https://itworksnowwhat.com'
+   else
+   rootOfPath = 'https://localhost:3000'
+
     const actionCodeSettings = {
         // Your redirect URL
-        url: `http://localhost:3000/passwordlessSignin?${redirectPath}`,
+
+   
+        url: `${rootOfPath}/passwordlessSignin?${redirectPath}`,
         handleCodeInApp: true
       };
 

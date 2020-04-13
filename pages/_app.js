ce934b050399e3b97173d1284cb241fd54e60fde
John Hashem <jphyqr@gmail.com>
2020-04-12 22:54:18 -0600

    added firestore provider

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,20 @@ import App from 'next/app';
 import { Provider } from "react-redux";
 import withRedux from "next-redux-wrapper";
 import { initializeStore } from '../config/store';
+import  firebase from '../firebase'
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import {createFirestoreInstance} from 'redux-firestore'
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  }
+
 
 class MyApp extends App {
+
+
+
+    
   static async getInitialProps({Component, ctx}) {
     //Preload from the server side
    
@@ -16,10 +28,22 @@ class MyApp extends App {
   }
   render() {
     const { Component, pageProps, store } = this.props;
+    
+   // const store = getOrInitializeStore(initialReduxState)
+   
+    const rrfProps = {
+      firebase,
+      config: rrfConfig,
+      dispatch: store.dispatch,
+       createFirestoreInstance // <- needed if using firestore
+    }
+    
     return (
       <Provider store={store}>
         <div>
+        <ReactReduxFirebaseProvider {...rrfProps}>
           <Component {...pageProps} />
+          </ReactReduxFirebaseProvider>
         </div>
       </Provider>
     );

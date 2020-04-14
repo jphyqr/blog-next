0cdf5bbbd5802f96afdfb50e4588972a2e245c3c
John Hashem <jphyqr@gmail.com>
2020-04-13 20:04:17 -0600

    go back to HOC now that api key fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 
-import withRedux from 'next-redux-wrapper';
+import {withRedux} from '../lib/redux';
 import { Provider } from 'react-redux';
 import { initializeStore } from '../config/store';
 import firebase from '../firebase'
@@ -8,26 +8,24 @@ import {createFirestoreInstance} from 'redux-firestore'
 function MyApp({ Component,  store, ...rest }) {
 
 
-    const rrfConfig = {
-        userProfile: 'users',
-        useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-      }
+    // const rrfConfig = {
+    //     userProfile: 'users',
+    //     useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+    //   }
 
-    const rrfProps = {
-        firebase,
-        config: rrfConfig,
-        dispatch: store.dispatch,
-         createFirestoreInstance // <- needed if using firestore
-      }
+    // const rrfProps = {
+    //     firebase,
+    //     config: rrfConfig,
+    //     dispatch: store.dispatch,
+    //      createFirestoreInstance // <- needed if using firestore
+    //   }
 
     return (
-        <Provider store={store}>
-              <ReactReduxFirebaseProvider {...rrfProps}>
+      
             <Component {...rest} />
-            </ReactReduxFirebaseProvider>
-        </Provider>
+         
     );
 }
 
 
-export default withRedux(initializeStore)(MyApp);
+export default withRedux(MyApp);
